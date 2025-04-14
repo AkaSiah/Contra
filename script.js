3e8b7d60c32523dcd130525f8a5d3d461508a4d4
Zydem <keoncummings11@gmail.com>
2025-04-14 18:18:01 -0400

    loading screen

--- script.js
+++ script.js
@@ -3,7 +3,12 @@
         const password = document.getElementById("password").value;
 
         if (username === "bob" && password === "bobpass") {
-            window.location.href = "index.html";
+            
+            document.querySelector(".wrapper").style.display = "none";
+            document.getElementById("loading-screen").style.display = "flex";
+            setTimeout(() => {
+                window.location.href = "index.html";
+            }, 2000);
         } else {
             alert("Invalid username or password.");
         }
