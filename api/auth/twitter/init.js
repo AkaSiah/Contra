1366967e394ad9e6cf0a1e907ce5a197852a9fbb
AkaSiah <josiahatican@gmail.com>
2025-04-21 13:34:56 -0400

    added my deployment

--- api/auth/twitter/init.js
+++ api/auth/twitter/init.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
   const params = new URLSearchParams({
     response_type: 'code',
     client_id: CLIENT_ID,
-    redirect_uri: 'https://your-deployment.vercel.app/api/auth/twitter/callback',
+    redirect_uri: 'https://contra-5hfqdq6wa-akasiahs-projects.vercel.app/api/auth/twitter/callback',
     scope: 'tweet.read tweet.write users.read bookmark.write',
     state,
     code_challenge: codeChallenge,
